ba33ae77a4f72ab9c997570b67c7861bcccc4fd4
PedroDutraSouza <147954687+PedroDutraSouza@users.noreply.github.com>
2025-06-30 17:50:25 +0000

    Add files via upload

---
+++ p10/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Pedro
+ * Date: 24/06/2025
+ * Time: 20:30
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace p10
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            string meuTexto = null; 
+            VerificarSeEhNulo(meuTexto);
+
+            meuTexto = "Olá, mundo!"; 
+            VerificarSeEhNulo(meuTexto);
+        }
+
+        static void VerificarSeEhNulo(string variavel)
+        {
+            
+            if (variavel == null)
+            {
+                Console.WriteLine("A variável é nula.");
+            }
+
+            else
+            {
+                Console.WriteLine($"A variável não é nula. Seu valor é: '{variavel}'");
+            }
+
+            string textoSeguro = variavel ?? "Valor Padrão (porque era nulo)";
+            Console.WriteLine($"Usando ??, o texto seguro é: '{textoSeguro}'\n");
+        }
+    }
+    
+}
---
+++ p9/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Pedro
+ * Date: 24/06/2025
+ * Time: 20:30
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace p9
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int dividendo = 10;
+            int divisor = 2;
+
+            if (dividendo % divisor == 0)
+            {
+                Console.WriteLine($"{dividendo} é divisível por {divisor}.");
+            }
+
+            else
+            {
+                Console.WriteLine($"{dividendo} não é divisível por {divisor}. O resto é {dividendo % divisor}.");
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
