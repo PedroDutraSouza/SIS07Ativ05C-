8de3af57f22369499468a2d9474bc4da8912c81b
PedroDutraSouza <147954687+PedroDutraSouza@users.noreply.github.com>
2025-06-30 17:48:40 +0000

    Add files via upload

---
+++ p5/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Pedro
+ * Date: 24/06/2025
+ * Time: 19:53
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace p5
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int idade = 20;
+            int idadeMinima = 18;
+
+            if (idade >= idadeMinima)
+            {
+                Console.WriteLine("A condição é verdadeira! A pessoa é maior de idade.");
+            }
+            else
+            {
+                Console.WriteLine("A condição é falsa! A pessoa é menor de idade.");
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ p6/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Pedro
+ * Date: 24/06/2025
+ * Time: 20:15
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace p6
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+          
+            double numero = -15.5;
+            double resultado;
+
+            resultado = Math.Abs(numero);
+
+            Console.WriteLine($"O valor absoluto de {numero} é {resultado}.");
+
+            Console.ReadKey(true);
+        }
+    }
+}
