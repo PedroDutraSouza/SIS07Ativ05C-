5ace288d6d7823e822987ec6dd47c6a758b97042
PedroDutraSouza <147954687+PedroDutraSouza@users.noreply.github.com>
2025-06-30 17:49:35 +0000

    Add files via upload

---
+++ p7/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Pedro
+ * Date: 24/06/2025
+ * Time: 20:20
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace p7
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            double numero = -10;
+
+            if (numero > 0)
+            {
+                Console.WriteLine("O número é positivo.");
+            }
+
+            else if (numero < 0)
+            {
+                Console.WriteLine("O número é negativo.");
+            }
+
+            else
+            {
+                Console.WriteLine("O número é zero.");
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ p8/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Pedro
+ * Date: 24/06/2025
+ * Time: 20:22
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace p8
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            string textoNumero = "123";
+            int numeroConvertido;
+
+            bool sucesso = int.TryParse(textoNumero, out numeroConvertido);
+
+            if (sucesso)
+            {
+                Console.WriteLine($"Conversão bem-sucedida! O número é: {numeroConvertido}");
+                Console.WriteLine($"Podemos agora fazer cálculos com ele: {numeroConvertido} * 2 = {numeroConvertido * 2}");
+            }
+            else
+            {
+                Console.WriteLine($"Falha na conversão. A string '{textoNumero}' não é um número inteiro válido.");
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
